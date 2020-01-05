abff5fa0e315f0352567e6a43adbf2794dab989e
Adi Fine <adif@Adis-MacBook-Pro.local>
2020-01-05 15:19:09 +0200

    added DIshPage

---
+++ frontend/src/containers/DishPage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import axios from 'axios';
+
+import {Card} from 'antd';
+import { List, Typography } from 'antd';
+import { Tag } from 'antd';
+import { Row, Col } from 'antd';
+
+
+const { Title } = Typography;
+
+class DishPage extends React.Component {
+
+    state = {
+        dish_id: 0,
+        dish_name: 'The New Noodle',
+        restaurant_name: 'Giraffe',
+        price: 58,
+        tags:['Asian', 'Vegan', 'shit'],
+        dish: {}
+    };
+
+    componentDidMount() {
+        this.setState({
+            dish_id: this.props.match.params.dishID
+        });
+    }
+
+    render() {
+        return(
+            <dom>
+                <Row>
+                    <Col span={12}>
+                        <Title level={2}>
+                            {this.state.dish_name}
+                        </Title>
+                        <Title level={3}>
+                            {this.state.restaurant_name}
+                        </Title>
+                        <h6 style={{marginRight: 0, display: 'inline' }}>Tags:</h6>
+                        <div>
+                            {this.state.tags.map((tag) =>
+            <Tag>{tag}</Tag>
+        )}
+                        </div>
+                        <List
+                            split={false}
+                            dataSource={this.state.tags}
+                            itemLayout={'horizontal'}
+                            renderItem={item => (
+                                <List.Item>
+                                    <Tag>{item}</Tag>
+                                </List.Item>
+                            )}
+                        />
+                    </Col>
+                    <Col span={12}>
+                        <Title level={3}>
+                            {this.state.price}
+                        </Title>
+                    </Col>
+                </Row>
+
+
+                <Card title={this.state.dish_id} >
+                    <p>{this.state.dish_id}</p>
+                </Card>
+            </dom>
+        )
+    }
+}
+export default DishPage;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,11 +3,13 @@ import { Route } from "react-router-dom";
 
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import DishPage from "./containers/DishPage";
 
 const BaseRouter = () => (
   <div>
     <Route exact path="/login/" component={Login} />{" "}
     <Route exact path="/signup/" component={Signup} />{" "}
+    <Route exact path="/dish/2" component={DishPage} />{" "}
   </div>
 );
 
