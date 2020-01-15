8fc113d09651aba6a41e20c02aa7d3cd49b7f825
chen31017 <chen31017@gmail.com>
2020-01-15 11:20:35 +0200

    .

--- backend/src/dishes/management/commands/stupid.py
+++ backend/src/dishes/management/commands/stupid.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from ...Engine.collaborative import update_estimations
 
 from ...Engine.EatWith import choose_restaurant
+from ...Engine.initialization import initialize, create_estimations, calculate_all_distance
 from ...Engine.search import is_legal_dish, search_dishes
-from ...models import Dish, Tag, City, CityArea
+from ...models import Dish, Tag, City, CityArea, TagDistances
 from ...Engine.collaborative import create_users_distances
 import numpy as np
 
@@ -12,12 +14,15 @@ class Command(BaseCommand):
     help = 'Displays current time'
 
     def handle(self, *args, **kwargs):
+        #update_estimations()
         #Tag.objects.create(title="Vegan")
         #Tag.objects.create(title="American")
-
-        tag = Tag.objects.get(title='Vegetarian')
+        #tag = Tag.objects.get(pk=3)
         area = CityArea.objects.get(pk=1)
-        user1 = User.objects.get(pk=1)
-        user2 = User.objects.get(pk=2)
-        print(choose_restaurant([(user1, [tag, Tag.objects.get(pk=2)]),(user2,[Tag.objects.get(title='Vegan'), Tag.objects.get(pk=2)])], area))
-        print(search_dishes(user=user1, area=CityArea.objects.get(pk=1), tags=[Tag.objects.get(title='Vegetarian')]))
+        #dish1 = Dish.objects.get(pk=1)
+        #print(choose_restaurant([(user1, [tag, Tag.objects.get(pk=2)]),(user2,[Tag.objects.get(title='Vegan'), Tag.objects.get(pk=2)])], area))
+        #print(search_dishes(user=user1, area=CityArea.objects.get(pk=1), tags=[Tag.objects.get(title='Vegetarian')]))
+        #create_estimations()
+        print(choose_restaurant([(User.objects.get(pk=1), [Tag.objects.get(title='Pizza')]), (User.objects.get(pk=2), [])], CityArea.objects.get(pk=1)))
+        print(TagDistances.objects.get(col__id=22, row__id=19).distance)
+        #print(search_dishes(User.objects.get(id=3), area, tags = [Tag.objects.get(id=1)], k=50))
