dd2132d78e360bb3043ec1953037a668e90cd895
orinL <orinlevy96@gmail.com>
2020-01-15 09:16:08 +0200

    commit to pull

--- backend/src/dishes/Engine/utils.py
+++ backend/src/dishes/Engine/utils.py
@@ -65,6 +65,165 @@ def k_multi_thread(target, args, k = 5):
 
 def create_tag_distances():
     for tag in Tag.objects.all():
+        TagDistances.objects.create(col=tag, row=tag, distance=0)
         children = tag.children.all()
         for child in children:
-            TagDistances.objects.create(child, tag, 0)
+            TagDistances.objects.create(col=child, row=tag, distance=0)
+
+
+def create_specific_distances():
+    level0 = Tag.objects.filter(title='All')
+    level1 = Tag.objects.filter(parent_id='37')
+    middle_east_cat = Tag.objects.filter(parent_id='22')
+    eropean_cat = Tag.objects.filter(parent_id='36')
+    general_cat = Tag.objects.filter(parent_id='19')
+    american_cat = Tag.objects.filter(parent_id='5')
+    asian_cats = Tag.objects.filter(parent_id='8')
+    deserts = Tag.objects.filter(parent_id='14')
+    greek_food = Tag.objects.filter(parent_id='23')
+    italian_food = Tag.objects.filter(parent_id='1')
+    japanease_food = Tag.objects.filter(parent_id='9')
+    chainese_food = Tag.objects.filter(parent_id='10')
+
+    # All with itsels:
+    TagDistances.objects.create(col=level0[0], row=level0[0], distance=0)
+    # All to its children and reverse:
+    for tag in level1:
+        TagDistances.objects.create(col=tag, row=level0[0], distance=3)
+        TagDistances.objects.create(col=level0[0], row=tag, distance=0)
+    # distances between level 2 objects
+    for tag1 in level1:
+        for tag2 in level1:
+            if tag1 == tag2:
+                TagDistances.objects.create(col=tag1, row=tag2, distance=0)
+            else:
+                TagDistances.objects.create(col=tag1, row=tag2, distance=3)
+
+    # middle east tags
+    middle_east = Tag.objects.get(title='Middle Eeast')
+    TagDistances.objects.create(col=middle_east, row=middle_east, distance=0)
+    for tag in middle_east_cat:
+        TagDistances.objects.create(col=tag, row=middle_east, distance=2)
+    greek = Tag.objects.get(title='Greek')
+    for tag1 in greek_food:
+        TagDistances.objects.create(col=tag1, row=greek, distance=1)
+        TagDistances.objects.create(col=tag1, row=tag1, distance=0)
+        for tag2 in greek_food:
+            TagDistances.objects.create(col=tag1, row=tag2, distance=1)
+
+    # Ero tags
+    ero = Tag.objects.get(title='Europenian')
+    TagDistances.objects.create(col=ero, row=ero, distance=0)
+    for tag in eropean_cat:
+        TagDistances.objects.create(col=tag, row=ero, distance=2)
+    italy = Tag.objects.get(title='Italian')
+    for tag1 in italian_food:
+        TagDistances.objects.create(col=tag1, row=italy, distance=1)
+        TagDistances.objects.create(col=tag1, row=tag1, distance=0)
+        for tag2 in italian_food:
+            TagDistances.objects.create(col=tag1, row=tag2, distance=1)
+
+    # General tags
+    general = Tag.objects.get(title='General Food')
+    TagDistances.objects.create(col=general, row=general, distance=0)
+    for tag1 in general_cat:
+        TagDistances.objects.create(col=tag1, row=general, distance=2)
+        TagDistances.objects.create(col=tag1, row=tag1, distance=0)
+        for tag2 in general_cat:
+            TagDistances.objects.create(col=tag1, row=tag2, distance=1)
+
+    # American tags
+    american = Tag.objects.get(title='American')
+    TagDistances.objects.create(col=american, row=american, distance=0)
+    for tag1 in american_cat:
+        TagDistances.objects.create(col=tag1, row=american, distance=2)
+        TagDistances.objects.create(col=tag1, row=tag1, distance=0)
+        for tag2 in american_cat:
+            TagDistances.objects.create(col=tag1, row=tag2, distance=1)
+
+    # Asian tags:
+    asian = Tag.objects.get(title='Asian')
+    TagDistances.objects.create(col=asian, row=asian, distance=0)
+    for tag in asian_cats:
+        TagDistances.objects.create(col=tag, row=asian, distance=2)
+
+    japan = Tag.objects.get(title='Japanese')
+    for tag1 in japanease_food:
+        TagDistances.objects.create(col=tag1, row=japan, distance=1)
+        TagDistances.objects.create(col=tag1, row=tag1, distance=0)
+        for tag2 in japanease_food:
+            TagDistances.objects.create(col=tag1, row=tag2, distance=1)
+
+    chaina = Tag.objects.get(title='Chainese')
+    for tag1 in chainese_food:
+        TagDistances.objects.create(col=tag1, row=chaina, distance=1)
+        TagDistances.objects.create(col=tag1, row=tag1, distance=0)
+        for tag2 in chainese_food:
+            TagDistances.objects.create(col=tag1, row=tag2, distance=1)
+
+
+    # Deserts tags
+    Desert = Tag.objects.get(title='Deserts')
+    TagDistances.objects.create(col=general, row=Desert, distance=0)
+    for tag1 in deserts:
+        TagDistances.objects.create(col=tag1, row=Desert, distance=2)
+        TagDistances.objects.create(col=tag1, row=tag1, distance=0)
+        for tag2 in deserts:
+            TagDistances.objects.create(col=tag1, row=tag2, distance=1)
+
+def create_another_tags_dists():
+    middle_east_cat = Tag.objects.filter(parent_id='22')
+    eropean_cat = Tag.objects.filter(parent_id='36')
+    general_cat = Tag.objects.filter(parent_id='19')
+    american_cat = Tag.objects.filter(parent_id='5')
+    asian_cats = Tag.objects.filter(parent_id='8')
+    deserts = Tag.objects.filter(parent_id='14')
+    greek_food = Tag.objects.filter(parent_id='23')
+    italian_food = Tag.objects.filter(parent_id='1')
+    japanease_food = Tag.objects.filter(parent_id='9')
+    chainese_food = Tag.objects.filter(parent_id='10')
+
+    all_tags = Tag.objects.all()
+
+    # middle east subtree
+    middle_east_subtree = [Tag.objects.get(title='Middle Eeast')]+list(middle_east_cat)+list(greek_food)
+    for tag_middle in middle_east_subtree:
+        for gen_tag in all_tags:
+            if gen_tag not in middle_east_subtree:
+                TagDistances.objects.create(col=tag_middle, row=gen_tag, distance=3)
+
+    # Ero subtree
+    Ero_subtree = [Tag.objects.get(title='Europenian')]+list(eropean_cat)+list(italian_food)
+    for tag_ero in Ero_subtree:
+        for gen_tag in all_tags:
+            if gen_tag not in Ero_subtree:
+                TagDistances.objects.create(col=tag_ero, row=gen_tag, distance=3)
+
+    #General Tags:
+    General_subtree = [Tag.objects.get(title='General Food')]+list(general_cat)
+    for tag_general in General_subtree:
+        for gen_tag in all_tags:
+            if gen_tag not in General_subtree:
+                TagDistances.objects.create(col=tag_general, row=gen_tag, distance=3)
+
+    #American Tags:
+    American_subtree = [Tag.objects.get(title='American')]+list(american_cat)
+    for American_tag in American_subtree:
+        for gen_tag in all_tags:
+            if gen_tag not in General_subtree:
+                TagDistances.objects.create(col=American_tag, row=gen_tag, distance=3)
+
+
+    # Asian subtree
+    Asian_subtree = [Tag.objects.get(title='Asian')]+list(asian_cats)+list(chainese_food)+list(japanease_food)
+    for tag_asian in Asian_subtree:
+        for gen_tag in all_tags:
+            if gen_tag not in Asian_subtree:
+                TagDistances.objects.create(col=tag_asian, row=gen_tag, distance=3)
+
+    #Desert Tags:
+    Desert_subtree = [Tag.objects.get(title='Deserts')]+list(deserts)
+    for Desert_tag in Desert_subtree:
+        for gen_tag in all_tags:
+            if gen_tag not in General_subtree:
+                TagDistances.objects.create(col=Desert_tag, row=gen_tag, distance=3)
--- backend/src/dishes/management/commands/stupid.py
+++ backend/src/dishes/management/commands/stupid.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
-from ...Engine.EatWith import choose_restaurant
+from ...Engine.utils import create_tag_distances, create_specific_distances
+from ...Engine.utils import create_another_tags_dists
 from ...Engine.search import is_legal_dish, search_dishes
 from ...models import Dish, Tag, City, CityArea
 from ...Engine.collaborative import create_users_distances
@@ -12,12 +13,6 @@ class Command(BaseCommand):
     help = 'Displays current time'
 
     def handle(self, *args, **kwargs):
-        #Tag.objects.create(title="Vegan")
-        #Tag.objects.create(title="American")
-
-        tag = Tag.objects.get(title='Vegetarian')
-        area = CityArea.objects.get(pk=1)
-        user1 = User.objects.get(pk=1)
-        user2 = User.objects.get(pk=2)
-        print(choose_restaurant([(user1, [tag, Tag.objects.get(pk=2)]),(user2,[Tag.objects.get(title='Vegan'), Tag.objects.get(pk=2)])], area))
-        print(search_dishes(user=user1, area=CityArea.objects.get(pk=1), tags=[Tag.objects.get(title='Vegetarian')]))
+        # create_tag_distances()
+        #create_specific_distances()
+        create_another_tags_dists()
