ba99dd660fa8f38eb617d06b4ca248480d77dfd6
chen31017 <chen31017@gmail.com>
2020-01-17 16:13:06 +0200

    search gets tag title instead of id

--- backend/src/dishes/engine/manage.py
+++ backend/src/dishes/engine/manage.py
@@ -4,21 +4,21 @@ from .eat_with import choose_restaurant
 from ..models import User, Dish, CityArea, Review, Tag
 
 
-def init__user(username): # TODO: make sure
+def init__user(username):
     user = User.objects.get(username=username)
     add_distances_for_new_user(user)
     add_estimations_for_new_user(user)
 
 
-def init__review(review_id): # TODO: if you need give me somthing else let me know
+def init__review(review_id): # TODO: if you need give me something else let me know
     review = Review.objects.get(id=review_id)
     update_distances_for_new_review(review)
 
 
-def search(username, areaname, tagsid = [], lte = None, gte = None):
+def search(username, area_name, tag_titles = [], lte = None, gte = None):
     user = User.objects.get(username=username)
-    area = CityArea.objects.get(name=areaname)
-    tags = [Tag.objects.get(id=tag_id) for tag_id in tagsid ]
+    area = CityArea.objects.get(name=area_name)
+    tags = [ Tag.objects.get(title=title) for title in tag_titles ]
 
     dishes = search_dishes(user, area, tags, lte, gte)
 
