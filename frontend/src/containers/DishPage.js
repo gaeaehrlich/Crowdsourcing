2b1650f28499d7ab182b3db80a8c79729007565f
orinL <orinlevy96@gmail.com>
2020-01-18 13:06:57 +0200

    DB fix

--- frontend/src/containers/DishPage.js
+++ frontend/src/containers/DishPage.js
@@ -1,30 +1,69 @@
 import React from "react";
 import axios from 'axios';
 
-import {Button, Icon, List, Comment, Rate} from 'antd';
-import { Typography } from 'antd';
-import { Tag , Drawer, Form, Input, Checkbox, } from 'antd';
-import { Row, Col } from 'antd';
-
+import {Button, Icon, Typography, Tag , Drawer, Row, Col} from 'antd';
+import ReviewForm from "../components/ReviewForm";
+import {connect} from "react-redux";
+import Reviews from "../components/Reviews";
+import  logo from "../smaller_logo.png"
 
 const { Title } = Typography;
-const { TextArea } = Input;
 
 class DishPage extends React.Component {
 
     state = {
         dish_id: 0,
-        dish_name: 'The New Noodle',
-        restaurant_name: 'Giraffe',
+        dish_name: '',
+        restaurant_name: '',
         address: '',
         price: 0,
-        tags:['Asian', 'Vegan', 'shit'],
+        tags:[],
         reviews: [],
         visible: false,
+        rest_id: null,
+        level: 1,
+        likes: [],
+        gifts: [],
+        searches: [],
+        preferences: []
+    };
+
+    updateUserSearch = (dishID) => {
+        axios.get(`http://127.0.0.1:8000/api/user/${this.props.token}/`)
+            .then(res => {
+            this.setState({
+                level: res.data.level,
+                likes: res.data.likes,
+                gifts: res.data.gifts,
+                searches: res.data.searches,
+                preferences: res.data.preferences
+            });
+            })
+            .catch(error => console.log(error));
+
+        axios.put(`http://127.0.0.1:8000/api/updateuser/${this.props.token}/`, {
+            user: this.props.token,
+            level: this.state.level,
+            likes: this.state.likes,
+            gifts: this.state.gifts,
+            searches: [...this.state.searches, dishID],
+            preferences: this.state.preferences
+        })
+            .then(res => console.log(res))
+            .catch(error => console.log(error));
+    };
+
+    fetchReviews = (dishID) => {
+        axios.get(`http://127.0.0.1:8000/api/dishreviews/${dishID}`).then(res => {
+            console.log(res.data);
+            this.setState({
+                reviews: res.data,
+            });
+        }).catch(error => console.log(error));
     };
 
     componentDidMount() {
-        const dishID = this.props.match.params.dishID
+        const dishID = this.props.match.params.dishID;
         this.setState({
             dish_id: dishID
         });
@@ -33,13 +72,15 @@ class DishPage extends React.Component {
             this.setState({
                 dish_name: res.data.title,
                 restaurant_name: res.data.restaurant.name,
-                // address: res.data.restaurant.street.name,
+                rest_id: res.data.restaurant.id,
                 price: res.data.price,
-                reviews: res.data.reviews,
-
+                tags: res.data.tags.map(tag=>(tag['title'])),
             });
-        });
-    }
+        }).catch(error => console.log(error));
+        this.updateUserSearch(dishID);
+        this.fetchReviews(dishID);
+    };
+
 
     showDrawer = () => {
         this.setState({
@@ -54,97 +95,71 @@ class DishPage extends React.Component {
 
     };
 
-    handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-      this.setState({
-            visible: false,
-        });
-    });
-  };
+    dishToPicLocation = name => {
+        let out;
+        out = name.replace(/ /g, '_');
+        return 'http://127.0.0.1:8000/api/pic/'+out;
+    };
+
 
     render() {
-        const { getFieldDecorator } = this.props.form;
-        return(
-            <dom>
+        return (
+            <div>
+
                 <Row>
-                    <Col span={12}>
-                        <Title level={2}>
-                            {this.state.dish_name}
+                    <Col span={16}>
+                        <Row>
+                            <Col span={16}>
+
+                                <Title level={2}>
+                                {this.state.dish_name}
+                            </Title>
+                            </Col>
+                            <Col span={8}>
+                                <Title level={3}>
+                            {this.state.price} ₪
                         </Title>
-                        <Title level={3}>
-                            {this.state.restaurant_name}
-                        </Title>
-                        <h6 style={{marginRight: 0, display: 'inline' }}>Tags:</h6>
-                        <div>
-                            {this.state.tags.map((tag) =>
-                                <Tag>{tag}</Tag>
-                            )}
-                        </div>
+                            </Col>
+
+                        </Row>
+                        <Row>
+                            <Title level={3}>
+                                <a href={'http://127.0.0.1:3000/rest/'+this.state.rest_id}>
+                                {this.state.restaurant_name}
+                            </a>
+                            </Title>
+                        </Row>
+                        <Row>
+                            <h6 style={{marginRight: 0, display: 'inline'}}>Tags:</h6>
+                            <div>
+                                {this.state.tags.map((tag) =>
+                                    <Tag>{tag}</Tag>
+                                )}
+                            </div>
+                        </Row>
                     </Col>
-                    <Col span={12}>
-                        <Title level={3}>
-                            {this.state.price}
-                        </Title>
+                    <Col span={8}>
+                        <img
+                            alt="So good.."
+                            src={this.dishToPicLocation(this.state.dish_name)}
+                            width="300"
+                        />
                     </Col>
+
                 </Row>
 
-                <Button type="primary" onClick={this.showDrawer} style={{marginTop:3}}>
-                    <Icon type="plus" /> Add review
+
+                <Button type="primary" onClick={this.showDrawer} style={{marginTop: 3}}>
+                    <Icon type="plus"/> Add review
                 </Button>
                 <Drawer
                     title="Add review"
                     width={360}
                     onClose={this.onClose}
                     visible={this.state.visible}
-                    bodyStyle={{ paddingBottom: 80 }}
+                    bodyStyle={{paddingBottom: 80}}
                 >
-                    <Form layout="vertical" hideRequiredMark onSubmit={this.handleSubmit}>
-
-                        <Row gutter={16}>
-
-                            <Form.Item label="Rate">
-                                {getFieldDecorator('rating', {
-                                    initialValue: 3,
-                                })(<Rate />)}
-                            </Form.Item>
-                        </Row>
-
-                        <Row gutter={16}>
-                            <Form.Item label="Review">
-                                {getFieldDecorator('review_text', {
-                                    rules: [{ required: true, message: 'Please enter a review' },]
-                                        // {validator:(rule, value, callback)=>{return value}}],
-                                })(
-                                    <TextArea
-                                        style={{ width: '100%' }}
-                                        placeholder="Please enter review"
-                                        autoSize={{ minRows: 3 }}
-                                    />,
-                                )}
-                            </Form.Item>
-                        </Row>
-                        <Row gutter={16}>
-                            <Form.Item>
-                                {getFieldDecorator('is_anonymous', {
-            valuePropName: 'checked',
-            initialValue: false,
-          }
-                                )(
-                                    <Checkbox>Post anonymously?</Checkbox>,
-                                )}
-                            </Form.Item>
-                        </Row>
-                        <Form.Item wrapperCol={{ span: 12 }}>
-          <Button type="primary" htmlType="submit" style={{position:"fixed", bottom:40}}>
-            Submit
-          </Button>
-        </Form.Item>
-
-                    </Form>
+                    <ReviewForm dishID={this.state.dish_id} token={this.props.token}/>
                     <div
                         style={{
                             position: 'absolute',
@@ -157,33 +172,21 @@ class DishPage extends React.Component {
                             textAlign: 'right',
                         }}
                     >
-                        {/*<Button onClick={this.onClose} style={{ marginRight: 8 }}>*/}
-                        {/*    Cancel*/}
-                        {/*</Button>*/}
-                        {/*<Button onClick={this.onClose} type="primary">*/}
-                        {/*    Submit*/}
-                        {/*</Button>*/}
                     </div>
                 </Drawer>
 
-                <List
-                    itemLayout="horizontal"
-                    dataSource={this.state.reviews}
-                    renderItem={review => (
-                        <li>
-                            <Comment
-                                // actions={item.actions}
-                                author={review.author.username}
-                                // avatar={item.avatar}
-                                content={review.description}
-                                // datetime={item.datetime}
-                            />
-                        </li>
-                    )}
-                />
-            </dom>
+                <Reviews data={this.state.reviews} token={this.props.token}/>
+            </div>
         )
     }
 }
-// export default DishPage;
-export default Form.create()(DishPage)
+
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.token !== null,
+        token: state.token
+    }
+};
+
+export default connect(mapStateToProps)(DishPage);
