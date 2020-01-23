c1a44371cb41c00affd01056f260dd39453e008e
chen31017 <chen31017@gmail.com>
2020-01-23 11:34:16 +0200

    clean house

--- backend/src/dishes/engine/utils.py
+++ backend/src/dishes/engine/utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 from ..models import DistanceMatrix, Tag, TagDistances
-import threading
 from django.contrib.auth.models import User
 
 
@@ -29,10 +28,9 @@ def get_distance(query_set):
 
 
 def related(tag1, tag2):
-    # tag1 is a son of tag2
     return tag1.tag_distances_from.get(row=tag2).distance == 0
 
-# every neighbors gave a review on the dish since all neighbors are from knn function
+
 def averaged_mean(user, dish, neighbors):
     neighbor_to_stars = lambda neighbor: dish.reviews.get(author_username=neighbor.username).stars
     neighbor_to_distance = lambda neighbor: neighbor.user_distances_to.get(col=user).distance
@@ -61,10 +59,3 @@ def create_fake_stars_list(user_reviews, other_reviews):
             array.append(0)
 
     return array
-
-
-# TODO: Orin - decide later where to use
-def k_multi_thread(target, args, k = 5):
-    t = threading.Thread(target=target, args=args)
-    t.start()
-    t.join()
